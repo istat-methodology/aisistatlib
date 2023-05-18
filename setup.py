bf47c8ca196dfd5f9cf147d4b7d1b9d436817d34
francescoamato <francamato@gmail.com>
2023-05-18 10:35:20 +0200

    first

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aisistatlib',
+    version='0.0.1',
+    author=['Francesco Amato', 'ISTAT'],
+    author_email='framato@istat.it',
+    description='Ais ISTAT wrapper functions',
+    packages = ['aisistatlib']
+)
+
+
